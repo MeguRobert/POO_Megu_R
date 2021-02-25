6f65395e7c67d007d1b8f83935e3e0ccbe6e5ba5
Megu Robi <megurobi14@outlook.com>
2021-02-25 15:45:42 +0200

    add Hello world project OOP style.

---
+++ 01-25 feb/01-25 feb/World.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01_25_feb
+{
+    class World
+    {
+        private int id;
+         public World(int id)
+         {
+            this.id = id;
+         }
+        public void SayHello()
+        {
+            Console.WriteLine($"HELLO from {id}");
+        }
+    }
+}
